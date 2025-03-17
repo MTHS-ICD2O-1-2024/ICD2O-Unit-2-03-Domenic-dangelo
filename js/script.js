a52a9bb80a16d31244ad677f0918707c3cca3c85
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-03-17 17:24:31 +0000

    street name and number

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: domenic dangelo
+// Created on: march 2025
 // This file contains the JS functions for index.html
 
 /**
- * Displays an alert with the message "Hello, World!"
+ * Do basic math.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function enterClicked() {
+  // input
+  const streetNumber = document.getElementById("street-number").value
+  const streetName = document.getElementById("street-name").value
+  // output
+  document.getElementById("address").innerHTML =
+    "Your address is: " + streetName + ", " + streetNumber + "."
 }
